53a5e5fe346bb0878dc4ff9a89f86736692b4374
lnzainn <syedbehzaadzainali@gmail.com>
2025-02-21 16:01:52 +0530

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import TimeTracker from "./TimeTracker";
+import "./styles.css";
 
-function App() {
+function HomePage() {
+  return (
+    <div className="container">
+      <div className="home-box">
+        <h1>Power Tools</h1>
+        <p>Welcome to Power Tools! Click below to go to the Time Tracker.</p>
+        <Link to="/timetracker" className="button">Go to Time Tracker</Link>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
   return (
     <Router basename="/power-tools">
       <Routes>
-        <Route path="/" element={<TimeTracker />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/timetracker" element={<TimeTracker />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
