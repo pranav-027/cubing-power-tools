1229f07da04d1f305905b13880790c35339b6552
lnzainn <syedbehzaadzainali@gmail.com>
2025-02-21 15:34:28 +0530

    Fix routing issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TimeTracker from "./TimeTracker";
 
 function App() {
   return (
-    <div className="App">
-      <TimeTracker />
-    </div>
+    <Router basename="/power-tools">
+      <Routes>
+        <Route path="/" element={<TimeTracker />} />
+      </Routes>
+    </Router>
   );
 }
 
